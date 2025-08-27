2a6ff77aef8582aa47c0ddc8d84958f5c451e7e5
Samuel Lara <slara@odin.cs.csubak.edu>
2025-08-27 11:39:24 -0700

    Add subtraction to app.cpp

--- app.cpp
+++ app.cpp
@@ -10,6 +10,6 @@ int main() {
     cout << "\nI'm using git!!! (WOOAAH)" << endl;
 
     cout << x << "plus" << y << " is " << x+y << endl;
-
+    cout << x << "minus" << y << " is " << x-y << endl;
     return 0;
 }
